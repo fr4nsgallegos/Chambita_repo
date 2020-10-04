7453451894f3c0c9f803adc06e18bb252cefabf5
Jhoel <jhuallpard@ulasalle.edu.pe>
2020-10-03 23:17:34 -0500

    Avance de Jhoel al 03-10-2020

---
+++ Avances - Jhoel/Aplicacion Web/Code/Trabajos/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { SocialAuthService } from 'angularx-social-login';
+import { SocialUser } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  AmazonLoginProvider,
+} from 'angularx-social-login';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+	constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+}
