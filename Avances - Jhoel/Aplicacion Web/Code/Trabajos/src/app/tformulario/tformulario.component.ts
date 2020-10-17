22016238c6113620ba8c474d12a1fb2ed9bd6151
Jhoel <jhuallpard@ulasalle.edu.pe>
2020-10-17 15:15:43 -0500

    Actualizacion 2.0

---
+++ Avances - Jhoel/Aplicacion Web/Code/Trabajos/src/app/tformulario/tformulario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tformulario',
+  templateUrl: './tformulario.component.html',
+  styleUrls: ['./tformulario.component.css']
+})
+export class TformularioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
